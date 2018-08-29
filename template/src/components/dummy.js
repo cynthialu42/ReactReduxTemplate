d2c38235c86f2938bb75e7de3fd840361f3495c6
cynthialu42 <clu2334@gmail.com>
2018-08-29 08:38:57 -0500

    boilerplate for react redux

---
+++ template/src/components/dummy.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Dummy extends Component {
+    render(){
+        return(
+            <div>Template Component</div>
+        )
+    }
+}
+
+export default Dummy;
